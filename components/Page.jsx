83176bf07aafd1270388456f189b2a1207636143
Icaro Albar <icaro.albar@live.com>
2022-10-26 09:57:00 -0300

    Estrutura geral

---
+++ components/Page.jsx
@@ -0,0 +1,19 @@
+import { NextSeo } from 'next-seo'
+
+export default function Page({ title, description, path, children }) {
+    const url = `http://planoassimrio.com.br${path}`
+    return (
+        <>
+            <NextSeo
+                title={title}
+                description={description}
+                canoncial={url}
+                openGraph={{
+                    url,
+                    title
+                }}
+            />
+            {children}
+        </>
+    )
+}
---
+++ components/SeoConfig.jsx
@@ -0,0 +1,7 @@
+const title = 'Plano de Saúde Assim Rio'
+const description = 'Todos os planos de saúde Assim e simulador de planos de saúde online'
+
+const SEO = {
+    title,
+    description
+}
